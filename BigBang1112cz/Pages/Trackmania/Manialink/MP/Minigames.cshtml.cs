6b3b6e3812e6c589889de723ce1d3bb88f4e3362
Petr Pivoňka <petrpiv1@gmail.com>
2025-05-03 19:35:30 +0200

    Redirect from querystring

--- BigBang1112cz/Pages/Trackmania/Manialink/MP/Minigames.cshtml.cs
+++ BigBang1112cz/Pages/Trackmania/Manialink/MP/Minigames.cshtml.cs
@@ -1,4 +1,6 @@
+using BigBang1112cz.Models.Trackmania.Manialink;
 using BigBang1112cz.Pages.Shared;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BigBang1112cz.Pages.Trackmania.Manialink.MP;
 
@@ -8,7 +10,20 @@ public class MinigamesModel : XmlPageModel
     {
     }
 
-    public void OnGet()
+    public IActionResult OnGet()
     {
+        var query = Request.QueryString.Value?.TrimStart('?');
+
+        if (string.IsNullOrEmpty(query))
+        {
+            return Page();
+        }
+
+        if (Enum.TryParse<Minigame>(query, ignoreCase: true, out var minigame))
+        {
+            return RedirectToPagePermanent($"/Trackmania/Manialink/MP/Minigames/{minigame}");
+        }
+
+        return NotFound();
     }
 }
