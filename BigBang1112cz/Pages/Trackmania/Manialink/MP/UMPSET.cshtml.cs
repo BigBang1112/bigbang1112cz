7f32804943e21c7e9f16987e080a85d2df656802
Petr Pivoňka <petrpiv1@gmail.com>
2025-05-03 20:16:53 +0200

    Add UMPSET manialink

---
+++ BigBang1112cz/Pages/Trackmania/Manialink/MP/UMPSET.cshtml.cs
@@ -0,0 +1,14 @@
+using BigBang1112cz.Pages.Shared;
+
+namespace BigBang1112cz.Pages.Trackmania.Manialink.MP;
+
+public class UMPSETModel : XmlPageModel
+{
+    public UMPSETModel(IHostEnvironment env) : base(env)
+    {
+    }
+
+    public void OnGet()
+    {
+    }
+}
