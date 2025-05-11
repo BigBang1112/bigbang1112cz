c5b45588bd5fb4bb85b24725487f535e47efdb14
Petr Pivoňka <petrpiv1@gmail.com>
2025-05-11 16:25:43 +0200

    Fix some nadeoenvimix manialink paths

---
+++ BigBang1112cz/Models/Trackmania/Manialink/NadeoEnvimixType.cs
@@ -0,0 +1,8 @@
+﻿namespace BigBang1112cz.Models.Trackmania.Manialink;
+
+public enum NadeoEnvimixType
+{
+    None,
+    About,
+    Privacy
+}
--- BigBang1112cz/Pages/Trackmania/Manialink/MP/NadeoEnvimix.cshtml.cs
+++ BigBang1112cz/Pages/Trackmania/Manialink/MP/NadeoEnvimix.cshtml.cs
@@ -1,14 +1,33 @@
+using BigBang1112cz.Models.Trackmania.Manialink;
 using BigBang1112cz.Pages.Shared;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BigBang1112cz.Pages.Trackmania.Manialink.MP;
 
 public class NadeoEnvimixModel : XmlPageModel
 {
+    public NadeoEnvimixType Type { get; set; }
+
     public NadeoEnvimixModel(IHostEnvironment env) : base(env)
     {
     }
 
-    public void OnGet()
+    public IActionResult OnGet()
     {
+        var query = Request.QueryString.Value?.TrimStart('?');
+
+        if (string.IsNullOrEmpty(query))
+        {
+            return Page();
+        }
+
+        if (!Enum.TryParse<NadeoEnvimixType>(query, ignoreCase: true, out var type))
+        {
+            return NotFound();
+        }
+
+        Type = type;
+
+        return Page();
     }
 }
